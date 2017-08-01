9b43c13c7f5cce99fe567a9984698837ad151efa
Subodh Jena <jenasubodh@gmail.com>
2017-08-01 13:09:02 +0530

    A little formatting

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -7,8 +7,8 @@ class MovieListItem extends Component {
         return(
             <View style={styles.container}>
                  <Image source={{uri: 'https://image.tmdb.org/t/p/w600' + this.props.movie.poster_path}}
-                        style={styles.imageBackground}>
-                        <Text style={styles.text}>{this.props.movie.title}</Text>
+                        style={styles.imageBackground} blurRadius={7}>
+                        <Text style={styles.text}>{this.props.movie.original_title}</Text>
                 </Image>
             </View>
         );
@@ -18,7 +18,9 @@ class MovieListItem extends Component {
 const styles = StyleSheet.create({
   container: {
     height: 250,
-    padding: 30
+    padding: 30,
+    flexDirection: 'row',
+    justifyContent: 'center'
   },
   imageBackground: {
     flex: 1,
@@ -27,10 +29,13 @@ const styles = StyleSheet.create({
     resizeMode: 'cover'
   },
   text: {
-    textAlign: 'center',
+    textAlign: 'left',
     color: 'white',
     backgroundColor: 'rgba(0,0,0,0)',
-    fontSize: 32
+    fontSize: 20,
+    paddingTop: 120,
+    paddingLeft: 10,
+    fontWeight: 'bold'
   }
 
 });
