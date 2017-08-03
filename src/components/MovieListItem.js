d317a0c877d3f71e5c7b2438134ce2c8b21e3df4
Subodh Jena <jenasubodh@gmail.com>
2017-08-03 16:30:31 +0530

    The movie list screen looks good now. More style changes

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -9,55 +9,60 @@ class MovieListItem extends Component {
 
     render() {
         return(
-            <TouchableHighlight
-              onPress={() => this.props.movieSelected(this.props.movie.id)}>
-              <View style={styles.container}>
-                  <Image source={{uri: 'https://image.tmdb.org/t/p/w600' + this.props.movie.poster_path}}
-                          style={styles.imageBackground} blurRadius={3}>
-                          <Text style={styles.textTitle}>{this.props.movie.original_title}</Text>
-                          <Text style={styles.textOverview}>{this.props.movie.overview}</Text>
-                  </Image>
-              </View>
+          <View style={styles.container}>
+            <TouchableHighlight onPress={() => this.props.movieSelected(this.props.movie.id)}>
+              <Image style={styles.movieImage} source={{uri: 'https://image.tmdb.org/t/p/w600' + this.props.movie.poster_path}}>
+                <View style={styles.movieDetailContainer}>
+                    <Text style={styles.movieName}>{this.props.movie.original_title}</Text>
+                    <Text style={styles.movieOverview}>{this.props.movie.overview}</Text>
+                </View>
+              </Image>
             </TouchableHighlight>
+        </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    borderRadius: 1,
-    shadowColor: "#000000",
-    shadowOpacity: 0.3,
-    paddingTop: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-    height: 250,
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-  imageBackground: {
-    flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'cover'
-  },
-  textTitle: {
-    textAlign: 'left',
-    color: 'white',
-    backgroundColor: 'rgba(0,0,0,0)',
-    fontSize: 20,
-    paddingTop: 120,
-    paddingLeft: 10,
-    fontWeight: 'bold'
-  },
-  textOverview: {
-    textAlign: 'left',
-    color: 'white',
-    backgroundColor: 'rgba(0,0,0,0)',
-    fontSize: 15,
-    paddingLeft: 10
-  }
+const styles = StyleSheet.create({ 
 
-});
+container: {
+  flex: 1,
+  paddingTop: 10,
+  paddingLeft: 10,
+  paddingRight: 10,
+  shadowColor: "#000",
+  shadowOpacity: 0.1,
+  shadowRadius: 1,
+  elevation: 5,
+},
+// View tag styling
+movieDetailContainer: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, .5)', 
+},
+// Image tag styling
+movieImage: {
+    width: null,
+    height: 250,
+    resizeMode: 'cover'
+},
+movieName: {
+    color: '#fff',
+    opacity: .9,
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: '500',
+    marginVertical: 15,
+    backgroundColor: 'transparent'
+},
+movieOverview: {
+    color: '#fff',
+    opacity: .9,
+    textAlign: 'center',
+    backgroundColor: 'transparent'
+}
+})
 
 export default MovieListItem;
